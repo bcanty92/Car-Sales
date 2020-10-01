2e0c377fd0d19381bb4ffffa220462030be224ef
Brittany Canty <bcanty92@gmail.com>
2020-10-01 03:25:56 -0500

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const AddedFeature = props => {
-  return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
-    </li>
-  );
+    return ( <
+        li > { /* Add an onClick to run a function to remove a feature */ } <
+        button className = "button"
+        onClick = {
+            () => props.removeFeature({ id: props.id, price: props.feature.price }) } >
+        X <
+        /button> { props.feature.name } <
+        /li>
+    );
 };
 
 export default AddedFeature;
